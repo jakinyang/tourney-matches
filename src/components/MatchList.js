c7012e7ee5d935dcd5b8aefbec1793cb927ea12b
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-27 20:00:56 -0800

    Compose components

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import Match from "./Match";
+import matchData from "../data/matchData"
 function MatchList() {
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      {/* Matches will be shown here */}
+      <Match />
     </section>
   );
 }
---
+++ src/helpers/playerHelpers.js
@@ -0,0 +1,7 @@
+const preparePlayerData = () => {
+  
+}
+
+const addWinsToPlayers = () => {
+
+}
